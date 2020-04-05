cc44bfc5b33ded295f59f1161518eb0a8358433c
edmundo papi <edmundopierre@yahoo.com>
2020-04-05 04:19:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,20 @@
+require 'pry'
 class Song
 
   attr_accessor :genre, :name, :artist
   @@genres =[]
   @@count = 0
   @@artists = []
-   def initialize(genre, name,artist)
+   def initialize(name,artist,genre)
      @name = name
      @genre = genre
      @artist = artist
      @@count +=1
-
-     if !@@genres.inlude?(self.genre)
-          @@genres << self.genre
-        end
-
-    if !@@artists.inlude?(self.artists)
-      @@artists << self.artists
-    end
+     @@genres << self.genre
+    @@artists << self.artist
 
 
+#binding.pry
    end
 
    def self.count
@@ -26,15 +22,33 @@ class Song
    end
 
    def self.genres
-     @@genres
+     @@genres.uniq!
    end
 
    def  self.artists
-      @@artists
+      @@artists.uniq!
    end
 
    def self.genre_count
 
+      hash = {}
+    @@genres.each do |genre|
+          if !hash[genre]
+            hash[genre] =1
+
+          else
+            hash[genre]+=1
+          end
+      #
+      # if !hash[self.genre].include?(self.genre)
+      #     hash[self.genre] = []
+      #     hash[self.genre] << self.genre.count
+      #
+      #      binding.pry
+    # if !has[self.genre].include?(self.genre)
+    #   hash[self.genre] << self.count
+    end
+    hash
    end
 
    def self.artist_count
